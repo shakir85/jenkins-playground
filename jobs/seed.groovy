1ccd688b43044844cc71d11b3c7cb085c398b13d
shakir <sralmasaari@gmail.com>
2022-12-14 11:07:13 -0800

    test

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -32,7 +32,7 @@ job('autopilot-dsl-job') {
 
     steps {
         shell("GREEN='\033[0;32m'; NC='\033[0m'")
-        shell("printf '${GREEN}Test DSL jobs! ${NC}'")
+        shell("printf '$GREEN Test DSL jobs! $NC'")
         shell("mvn --version")
     }
 }
