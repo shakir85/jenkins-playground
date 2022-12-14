a03f55ab9b0647dec3d4f52f19b11ee4512a9a13
shakir <sralmasaari@gmail.com>
2022-12-14 09:26:05 -0800

    test

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,32 +1,6 @@
-// Seed scripts to generate Jenkins pipelines as code.
-// This script is triggered by an upstream freestyle job: 'seed'
-// and generates various downstream pipeline-jobs for each 'pipelineJob' block below.
-//
-// Pipeline blocks are idempotent + removning a job doesn't delete it from Jenkins (no jobs state mgmt).
-pipelineJob('simple-java-maven-app') {
-    definition {
-        cpsScm {
-            scm {
-                git('https://github.com/shakir85/simple-java-maven-app.git')
-                branch('main')
-                git {
-                    extensions {
-                        cloneOptions {
-                            noTags(noTags = true)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
 pipelineJob('Hello-world') {
-    definition {
-        //description("Hello world job")
+    definition {    
         cps {
-            // path to Jenkins file this pipeline is going to use for builds 
-            // (path is relative to the seed workspace path).
             script(readFileFromWorkspace('jenkins-playground/pipelines/helloWorld/Jenkinsfile'))
             sandbox()
         }
