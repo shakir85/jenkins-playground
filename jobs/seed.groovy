776c983cac6feef8fb49458bb17073815ec05060
shakir <sralmasaari@gmail.com>
2022-12-14 10:19:14 -0800

    Add param

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,6 +1,9 @@
 pipelineJob('Hello-world') {
     definition {   
         description("Hello world job") 
+        parameters{
+            stringParam('myParameterName', 'my default stringParam value', 'my description')
+        }
         cps {
             script(readFileFromWorkspace('jenkins-playground/pipelines/helloWorld/Jenkinsfile'))
             sandbox()
