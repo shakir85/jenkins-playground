4eba2c160b6f76a7b8eddb29e5b685745a1b01e5
shakir <sralmasaari@gmail.com>
2022-12-14 10:48:36 -0800

    Restore params

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,13 +1,13 @@
-parameters {
-    stringParam('myParameterName', 'my default stringParam value', 'my description')
-    choiceParam('deployParam', ['dev (default)', 'test', 'prod'], 'Select deploy environment')
-        booleanParam('Required', false)
-}
-
 pipelineJob('Hello-world') {
     definition {
 
         description("Hello world job") 
+
+        parameters {
+            stringParam('myParameterName', 'my default stringParam value', 'my description')
+            choiceParam('deployParam', ['dev (default)', 'test', 'prod'], 'Select deploy environment')
+            booleanParam('Required', false)
+        }
         
         cps {
             script(readFileFromWorkspace('jenkins-playground/pipelines/helloWorld/Jenkinsfile'))
