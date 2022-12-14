727e825a7cbec28bf6d8f564e386d4d0bc9120c5
shakir <sralmasaari@gmail.com>
2022-12-14 10:33:51 -0800

    Add shell exec step

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,10 +1,17 @@
 pipelineJob('Hello-world') {
     definition {   
         description("Hello world job") 
+        
         parameters{
             stringParam('myParameterName', 'my default stringParam value', 'my description')
             choiceParam('deployParam', ['dev (default)', 'test', 'prod'], 'Select deploy environment')
+             booleanParam('Required', false)
         }
+
+        steps{
+            shell("echo 'This is a hello world project'")
+        }
+
         cps {
             script(readFileFromWorkspace('jenkins-playground/pipelines/helloWorld/Jenkinsfile'))
             sandbox()
