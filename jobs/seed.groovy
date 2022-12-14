6f95809e867ae051309d8c169aa99fa5cf0b9dab
shakir <sralmasaari@gmail.com>
2022-12-14 10:38:08 -0800

    Fix build exec

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -2,14 +2,14 @@ pipelineJob('Hello-world') {
     definition {   
         description("Hello world job") 
         
-        parameters{
+        parameters {
             stringParam('myParameterName', 'my default stringParam value', 'my description')
             choiceParam('deployParam', ['dev (default)', 'test', 'prod'], 'Select deploy environment')
              booleanParam('Required', false)
         }
 
-        steps{
-            shell("echo 'This is a hello world project'")
+        preBuildSteps {
+            shell("echo 'this is a hello world step' >> file")
         }
 
         cps {
