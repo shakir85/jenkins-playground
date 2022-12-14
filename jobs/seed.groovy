4bdb6b63830434e5772396541299adafdf1f21a6
shakir <sralmasaari@gmail.com>
2022-12-14 10:46:43 -0800

    Test glob

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,16 +1,15 @@
+parameters {
+    stringParam('myParameterName', 'my default stringParam value', 'my description')
+    choiceParam('deployParam', ['dev (default)', 'test', 'prod'], 'Select deploy environment')
+        booleanParam('Required', false)
+}
+
 pipelineJob('Hello-world') {
     definition {
 
         description("Hello world job") 
         
-        parameters {
-            stringParam('myParameterName', 'my default stringParam value', 'my description')
-            choiceParam('deployParam', ['dev (default)', 'test', 'prod'], 'Select deploy environment')
-             booleanParam('Required', false)
-        }
-
         cps {
-            script("echo 'hello world proj' >> file")
             script(readFileFromWorkspace('jenkins-playground/pipelines/helloWorld/Jenkinsfile'))
             sandbox()
         }
