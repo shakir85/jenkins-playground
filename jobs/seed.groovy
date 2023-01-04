e54fbc3ca7cea433d6c1cbae33d901e39cb583c5
shakir <sralmasaari@gmail.com>
2023-01-04 08:45:25 -0800

    Disable all

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,43 +1,43 @@
-pipelineJob('Hello-world') {
-    definition {
+// pipelineJob('Hello-world') {
+//     definition {
 
-        description("Hello world job") 
+//         description("Hello world job") 
 
-        parameters {
-            stringParam('myParameterName', 'my default stringParam value', 'my description')
-            choiceParam('deployParam', ['dev (default)', 'test', 'prod'], 'Select deploy environment')
-            booleanParam('Required', false)
-        }
+//         parameters {
+//             stringParam('myParameterName', 'my default stringParam value', 'my description')
+//             choiceParam('deployParam', ['dev (default)', 'test', 'prod'], 'Select deploy environment')
+//             booleanParam('Required', false)
+//         }
         
-        cps {
-            script(readFileFromWorkspace('jenkins-playground/pipelines/helloWorld/Jenkinsfile'))
-            // sandbox()
-        }
-    }
-}
+//         cps {
+//             script(readFileFromWorkspace('jenkins-playground/pipelines/helloWorld/Jenkinsfile'))
+//             // sandbox()
+//         }
+//     }
+// }
 
-job('autopilot-dsl-job') {
+// job('autopilot-dsl-job') {
 
-    description('Testing DSL jobs')
+//     description('Testing DSL jobs')
 
-    displayName('Auto-Pilot DSL Job')
+//     displayName('Auto-Pilot DSL Job')
     
-    parameters{
-        stringParam('Tool', defaultValue='mvn', description='Version check tool')
-        choiceParam('Args', ['--version (default)', '--help'])
-    }
+//     parameters{
+//         stringParam('Tool', defaultValue='mvn', description='Version check tool')
+//         choiceParam('Args', ['--version (default)', '--help'])
+//     }
 
-    wrappers {
-        colorizeOutput()
-        timestamps()
-    }
+//     wrappers {
+//         colorizeOutput()
+//         timestamps()
+//     }
 
-    scm {
-        git('https://github.com/shakir85/simple-java-maven-app.git', 'master')
-    }
+//     scm {
+//         git('https://github.com/shakir85/simple-java-maven-app.git', 'master')
+//     }
 
-    steps {
-        shell("echo 'Test DSL jobs!'")
-        shell("mvn --version")
-    }
-}
+//     steps {
+//         shell("echo 'Test DSL jobs!'")
+//         shell("mvn --version")
+//     }
+// }
