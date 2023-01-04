076507c45b6196639b578bbcd5c9f69058b5f9ac
shakir <sralmasaari@gmail.com>
2023-01-04 10:02:58 -0800

    Move attr to job block

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,19 +1,19 @@
-environmentVariables {
+pipelineJob("simple-java-maven-app-${env.version}") {
+    environmentVariables {
         env('version', '01')
     }
 
-folder = "myapps-${env.version}/simple-mvn-app"
-folder(folder) { 
+    folder = "myapps-${env.version}/simple-mvn-app"
+    folder(folder) { 
         displayName("gen-simple-mvn-app")
     }
 
-pipelineJob("simple-java-maven-app-${env.version}") {
     definition {
         cpsScm {
             scm {
                 git('git@github.com:shakir85/simple-java-maven-app.git')
             }
-        }
+        }   
         scriptPath('jenkins/Jenkinsfile')
     }
 }
