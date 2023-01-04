c0f3c3f7139272ad6de8421e6ff361d7a703f2a8
shakir <sralmasaari@gmail.com>
2023-01-04 12:32:17 -0800

    Hack to disable git-plugin tagging

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -2,15 +2,7 @@ pipelineJob("simple-java-maven-app") {
     definition {
         cpsScm {
             scm {
-                git('https://github.com/shakir85/simple-java-maven-app.git')
-                git {
-                    extensions {
-                        perBuildTag(False)
-                        // cloneOption {
-                        //     noTag(True)
-                        // }
-                    }
-                }
+                git('https://github.com/shakir85/simple-java-maven-app.git', 'master', {node -> node / 'extensions' << '' })
             }
             scriptPath('jenkins/Jenkinsfile')
         }   
