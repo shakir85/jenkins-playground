429a66e340d0779c674c2787a015da0587d28786
Shakir <35549045+shakir85@users.noreply.github.com>
2023-04-27 12:43:32 -0700

    update repo

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -3,7 +3,7 @@ pipelineJob("simple-java-maven-app") {
         cpsScm {
             scm {
                 // the 3rd attribute is a hack to disable Jenkins DSL git-tagging
-                git('https://github.com/gilesgas/simple-java-maven-app-original', 'master', {node -> node / 'extensions' << '' })
+                git('https://github.dev/shakir85/simple-java-maven-app-original', 'master', {node -> node / 'extensions' << '' })
                 //git('https://github.com/shakir85/simple-java-maven-app.git', 'master', {node -> node / 'extensions' << '' })
             }
             scriptPath('jenkins/Jenkinsfile')
