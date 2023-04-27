7d3e8c4dcd7f25a368417c40bbf556fd296abab1
shakir <sralmasaari@gmail.com>
2023-04-27 12:29:55 -0700

    update app repo

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,16 +1,10 @@
 pipelineJob("simple-java-maven-app") {
-        environmentVariables {
-            env('ONE', '1')
-            env('TWO', '2')
-        }
-        blockOn(['project-1']) { blockLevel('GLOBAL') }
-        parameters { stringParam('VERSION') }
-
     definition {
         cpsScm {
             scm {
                 // the 3rd attribute is a hack to disable Jenkins DSL git-tagging
-                git('https://github.com/shakir85/simple-java-maven-app.git', 'master', {node -> node / 'extensions' << '' })
+                git('https://github.com/gilesgas/simple-java-maven-app-original', 'master', {node -> node / 'extensions' << '' })
+                //git('https://github.com/shakir85/simple-java-maven-app.git', 'master', {node -> node / 'extensions' << '' })
             }
             scriptPath('jenkins/Jenkinsfile')
         }   
